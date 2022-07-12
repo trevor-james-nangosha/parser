8cf2425fe0e9b0a07f2c97a1cafe0d4f60c00a37
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-07-12 18:51:26 +0300

    playing around with the arguments to the parse function

--- run.js
+++ run.js
@@ -1,6 +1,6 @@
 const { Parser } = require('./Parser')
 const parser = new Parser()
 
-const program = 42
+const program = '43'
 const ast = parser.parse(program)
 console.log(JSON.stringify(ast, null, 4))
